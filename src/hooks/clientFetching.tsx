8e5706746ea0d023945502c0b0cc385023993519
jeanbutar24 <jeanbutar24@gmail.com>
2023-09-01 15:17:28 +0700

    perf: update how refresh token work

--- src/hooks/clientFetching.tsx
+++ src/hooks/clientFetching.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { useEffect } from 'react';
 import { axiosAuth } from '@/lib/api';
 import { getCookie } from 'cookies-next';
 
@@ -8,40 +7,34 @@ const ClientFetching = () => {
    const access_token = getCookie('access_token');
    const refresh_token = getCookie('refresh_token');
 
-   useEffect(() => {
-      const requestIntrercept = axiosAuth.interceptors.request.use((config) => {
-         if (!config.headers['Authorization']) {
-            config.headers['Authorization'] = `Bearer ${access_token}`;
-         }
-         return config;
-      });
-      const responseIntercept = axiosAuth.interceptors.response.use(
-         (response) => {
-            return response;
-         },
-         async (error) => {
-            if (error.response.status == 401) {
-               try {
-                  const originalRequest = error.config;
-                  const refresh = await axiosAuth.post(
-                     '/delivery/auth/login/refresh',
-                     { refresh_token }
-                  );
-                  axiosAuth.defaults.headers.common['Authorization'] =
-                     'Bearer ' + refresh.data.access_token;
-                  return axiosAuth(originalRequest);
-               } catch (error: any) {
-                  return Promise.reject(error);
-               }
+   axiosAuth.interceptors.request.use((config) => {
+      if (!config.headers['Authorization']) {
+         config.headers['Authorization'] = `Bearer ${access_token}`;
+      }
+      return config;
+   });
+   axiosAuth.interceptors.response.use(
+      (response) => {
+         return response;
+      },
+      async (error) => {
+         if (error.response.status == 401) {
+            try {
+               const originalRequest = error.config;
+               const refresh = await axiosAuth.post(
+                  '/delivery/auth/login/refresh',
+                  { refresh_token }
+               );
+               axiosAuth.defaults.headers.common['Authorization'] =
+                  'Bearer ' + refresh.data.access_token;
+               return axiosAuth(originalRequest);
+            } catch (error: any) {
+               return Promise.reject(error);
             }
-            throw error;
          }
-      );
-      return () => {
-         axiosAuth.interceptors.request.eject(requestIntrercept);
-         axiosAuth.interceptors.request.eject(responseIntercept);
-      };
-   }, [access_token, refresh_token]);
+         throw error;
+      }
+   );
 
    return axiosAuth;
 };
